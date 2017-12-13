e1c255a16d6d1408767c48641608e045faa2956e
RaHsu <rahsuchina@163.com>
2017-12-13 14:12:56 +0800

    v 0.2.1

---
+++ modules/json.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+// 读取配置文件配置并将其转换为js对象
+exports.readConfig = function (filename) {
+    let data = fs.readFileSync(filename);
+    return JSON.parse(data);
+}
+
+
+// 写入配置文件
--- test.js
+++ test.js
@@ -1,43 +1,4 @@
-var md = require("markdown-it")();
-var fs = require("fs");
+const json = require('./modules/json');
+const config = json.readConfig('package.json');
 
-//生成目录
-
-var href,name,a;
-fs.readdir('source/',function(err,files){
-	var dir = "";
-	for(let i = 0;i<files.length;i++){
-		
-		if(files[i].slice(-3,-1) === '.m'){
-			href = files[i].slice(0,-3)+'.html';
-			name = files[i].slice(0,-3);
-			a = `<a href="${href}">${name}</a></br>`;
-			
-			dir+=a;
-		}
-		
-
-	}
-	//console.log('dir:'+dir);
-
-var index = `<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="stylesheets/apollo.css">
-</head>
-<body>
-	${dir}
-</body>
-</html>`;
-//console.log(index);
-	// 写入目录
-	fs.writeFile('publish/index.html',index,function(err,data){
-		if(err){
-			console.log('目录文件写入失败');
-		}
-		else{
-			console.log('目录文件成功生成！');
-		}
-	})
-})
+console.log(config.name);
