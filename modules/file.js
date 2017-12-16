6cbc3712508f1fe68c700143196116d09c5df14f
RaHsu <rahsuchina@163.com>
2017-12-16 16:53:09 +0800

    新增 writeMd函数

--- modules/file.js
+++ modules/file.js
@@ -5,7 +5,7 @@ const re = require("./re");
 // 读取配置文件并解析配置项
 const prime_config = json.readConfig('site-config.json');
 const config = json.analysis_config(prime_config);
-console.log(prime_config);
+
 exports.writeMd = function(){
      //首先读取模板文件
      let template = fs.readFileSync("./../Radoc/static/template/md.html");
