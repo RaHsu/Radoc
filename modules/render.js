b31716bad56d5a6f50386a214a318b1615367363
RaHsu <rahsuchina@163.com>
2018-03-05 15:42:13 +0800

    fix 修复了一些错误

--- modules/render.js
+++ modules/render.js
@@ -1,7 +1,29 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
+const home_config = require('./../home-config.json');
+
+let template = {
+    basic:{
+        p:'<p></p>',
+    },
+    header:{
+        self:'<header id="header"></header>',
+        logo:'<div class="logo"></div>'
+    }
+};
 
 // 将模板加载为dom树
 let $ = cheerio.load(fs.readFileSync('./../static/template/home.html'));
 
-console.log($.html());
+
+// 对各个模块的解析
+// 对header部分的解析
+(function hearderAnalysis(){
+    if(home_config.header){
+        if(home_config.header.logo){
+            console.log("logo");
+        }
+    }
+})();
+$('body').append(template.header.self);
+//console.log(home_config);
