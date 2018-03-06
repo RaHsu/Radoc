d2c283569cee23e58d5703faa7069fa369ff4b80
RaHsu <rahsuchina@163.com>
2018-03-06 21:02:29 +0800

    feat header-links部分的解析

--- modules/render.js
+++ modules/render.js
@@ -7,11 +7,16 @@ let template = {
         p:'<p></p>',
         img:'<img>',
         i:'<i></i>',
-        span:'<span></span>'
+        span:'<span></span>',
+        a:'<a></a>',
+        ul:'<ul></ul>',
+
     },
     header:{
         self:'<header id="header"></header>',
-        logo:'<div class="logo header-left"></div>'
+        logo:'<div class="logo header-left"></div>',
+        links:'<div class="links"></div>',
+        dropdown:'<div class="dropdown"></div>'
     }
 };
 
@@ -25,33 +30,75 @@ function setHeader(){
     if(home_config.header){
         $('body').append(template.header.self);
         setHeaderLogo();
+        setHeaderLinks();
     }
 
 }
+
 // 对header中logo的解析
 function setHeaderLogo(){
     if(home_config.header.logo){
         let logo = home_config.header.logo;
         $('#header').append(template.header.logo);
-        for(let i in logo){
-            if(logo[i].type === 'img'){
+        for(let i of logo){
+            if(i.type === 'img'){
                 $('#header .logo').append(template.basic.img);
-                $('#header .logo img').attr('src',logo[i].src);
+                $('#header .logo img').attr('src',i.src);
             }
-            if(logo[i].type === 'icon'){
+            if(i.type === 'icon'){
                 $('#header .logo').append(template.basic.i);
-                $('#header .logo i').addClass('fa fa-' + logo[i].icon_name);
-                if(logo[i].color){
-                    $('#header .logo i').attr('style','color:' + logo[i].color + ';');
+                $('#header .logo i').addClass('fa fa-' + i.icon_name);
+                if(i.color){
+                    $('#header .logo i').attr('style','color:' + i.color + ';');
                 }
             }
-            if(logo[i].type === 'text'){
+            if(i.type === 'text'){
                 $('#header .logo').append(template.basic.span);
-                $('#header .logo span').text(logo[i].content);
+                $('#header .logo span').text(i.content);
             }
         }
     }
 
+}
+
+// 对header中links的解析
+function setHeaderLinks(){
+    if(home_config.header.links){
+        let links = home_config.header.links;
+        $('#header').append(template.header.links);
+        // 对position进行解析
+        if(links.position){
+            if(links.position === 'left'){
+                $('#header .links').addClass('header-left');
+            }else if (links.position === 'right') {
+                $('#header .links').addClass('header-right');
+            }
+        }
+        // 对链接的解析
+        if(links.children){
+            for(let i of links.children){
+                // 对单链接的解析
+                if(i.type === 'link'){
+                    let a = `<a href="${i.href}">${i.text}</a>`;
+                    $('#header .links').append(a);
+                }
+                // 对下拉菜单的解析
+                if(i.type === 'dropdown'){
+                    $('#header .links').append(template.header.dropdown);
+                    let dropdown = `<a class="dropdown-nav">${i.text}</a>`;
+                    $('#header .links .dropdown').append(dropdown);
+                    $('#header .links .dropdown').append(template.basic.ul);
+                    for(let children of i.children){
+                        console.log(children);
+                        let a = `<li><a href="${children.href}">${children.text}</a></li>`;
+                        $('#header .links .dropdown ul').append(a);
+                    }
+                }
+
+            }
+        }
+
+    }
 }
 
 setHeader();
