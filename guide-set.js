fc2cce7c4561234d20f80c7f462306d7dd68b070
RaHsu <rahsuchina@163.com>
2019-02-01 21:14:20 +0800

    fea 设置了文档页的内容高度

--- guide-set.js
+++ guide-set.js
@@ -1,1 +1,9 @@
-var contianerHeight = document.getElementById('container');
+// container高度设置
+var contianerHeight = document.getElementById('container').style.height;
+var clientHeight = window.innerHeight;
+
+document.getElementById('sidebar').style.height = clientHeight - 110 + 'px';
+document.getElementById('container').style.height = clientHeight - 110 + 'px';
+document.getElementById('doc').style.height = clientHeight - 100 + 'px';
+
+console.log(clientHeight);
