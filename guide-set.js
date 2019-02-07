e3bacaf231b813e6c3f66578affe4af070ffd103
RaHsu <rahsuchina@163.com>
2019-02-07 21:28:55 +0800

    fix 修改和添加一些功能

--- guide-set.js
+++ guide-set.js
@@ -6,6 +6,22 @@ document.getElementById('sidebar').style.height = clientHeight - 110 + 'px';
 document.getElementById('container').style.height = clientHeight - 110 + 'px';
 document.getElementById('doc').style.height = clientHeight - 100 + 'px';
 
+// 点击链接ifream中出现文档以及文字高亮
+var links = document.getElementsByClassName('doc-link');
+for(var i = 0;i < links.length;i++){
+    console.log(links[i].getAttribute('data-src'));
+    links[i].onclick = function(e){
+        document.getElementById('doc').src = this.getAttribute('data-src');
+        // 使其他所有的active标签失灵
+        for(var j =0;j < links.length;j++){
+            links[j].className = 'doc-link';
+        }
+        this.classList.add('active');
+        // 使父元素高亮
+        
+    }
+}
+
 // 使文章与目录对应
 var docName =  document.getElementById('doc').src;
 var htmlRxp = /publish\/\S*.html/img;
